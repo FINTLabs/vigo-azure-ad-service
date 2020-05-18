ab7afbb64735657d514a7b8c7b948a6df9e3b0ca
Frode <frode@fintlabs.no>
2020-05-18 12:16:51 +0200

    changed default prop

--- src/main/java/no/vigo/Props.java
+++ src/main/java/no/vigo/Props.java
@@ -28,7 +28,7 @@ public class Props {
     @Value("${fint.azure.qlik.users.owner:hans@vigoikt.no}")
     private String qlikUsersOwner;
 
-    @Value("${fint.azure.qlik.redirect-url:https://qs.fintlabs.no}")
+    @Value("${fint.azure.qlik.redirect-url:https://qlik.fintlabs.no}")
     private String qlikRedirectUrl;
 
     @Value("${fint.azure.qlik.send-invitation:false}")
