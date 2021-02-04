397a3ce433b20408952e2962bf3b5d0614adbf0f
OlaStalberg <olastaalberg@gmail.com>
2021-02-04 14:53:46 +0100

    changing manager for the purpose that all logged inn can invite

--- src/main/java/no/vigo/azure/ad/UserController.java
+++ src/main/java/no/vigo/azure/ad/UserController.java
@@ -2,6 +2,7 @@ package no.vigo.azure.ad;
 
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+import no.vigo.Props;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,14 +17,17 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final Props props;
 
-    public UserController(UserService userService) {
+
+    public UserController(UserService userService, Props props) {
         this.userService = userService;
+        this.props = props;
     }
 
     @GetMapping
     public ResponseEntity<List<JsonObject>> getMyUser(@RequestHeader(name = "x-upn") String upn) {
-        List<JsonObject> usersByManager = userService.getUsersByManager(upn);
+        List<JsonObject> usersByManager = userService.getUsersByManager(props.getQlikUsersOwner());
         return ResponseEntity.ok(usersByManager);
     }
 }
